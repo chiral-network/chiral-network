43a505d38a8a914f4c4aed34163f04f36b09957a
GreenMarioX <steventung666@gmail.com>
2026-01-31 20:52:21 -0500

    Chiral Rebuild Template

---
+++ v2-chiral-network/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+const host = process.env.TAURI_DEV_HOST;
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: 'ws',
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ['**/src-tauri/**'],
+    },
+  },
+});
